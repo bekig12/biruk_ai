f97592976d6ff64a6582f63509bad850938f70bd
beki <bereketgrima108@gmail.com>
2026-02-09 00:11:46 +0300

    Change require to import for formidable and fs

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ import cors from "cors";
 import fetch from "node-fetch";
 import path from "path";
 import { fileURLToPath } from "url";
-const formidable = require("formidable");
-const fs = require("fs");
+const formidable = import("formidable");
+const fs = import("fs");
 
 
 // TTS helper
