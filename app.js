a9912cbf7b35d84103220c97c5edbfd08d2ebfa1
beki <bereketgrima108@gmail.com>
2026-02-09 00:06:20 +0300

    Add formidable and fs modules to app.js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import fetch from "node-fetch";
 import path from "path";
 import { fileURLToPath } from "url";
+const formidable = require("formidable");
+const fs = require("fs");
+
+
 // TTS helper
 import ffmpeg from "fluent-ffmpeg";
 import { PassThrough } from "stream";
